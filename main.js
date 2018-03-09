70e7de1fe2c1858935b377161ae61c1149438322
Mehret Kulala <merutaye@gmail.com>
2018-03-09 17:29:50 -0600

    compeleted challenge2

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ console.log ("Hello!");
 
 //challenge 1
 
-//challenge 1
-
-// challenge 1
 
 var players = [{ name: "Bob", scores: [10, 65] }, { name: "Bill", scores: [90, 5] }, { name: "Charlie", scores: [40, 55] }, {name: "Rick Sanchez", scores:[99,0]}]; // Displays "Bill"
 
@@ -40,3 +37,41 @@ if (maxTotal === 0) {
 }
 
 challenge1.innerHTML += "<h3>"+winner+"</h3>";
+
+
+//challenge 2
+
+var winnerList = [
+    {season: '1999-2000', team: 'Real Madrid', country: 'Spain'},
+    {season: '2000-2001', team: 'Bayern Munich', country: 'Germany'},
+    {season: '2001-2002', team: 'Real Madrid', country: 'Spain'},
+    {season: '2002-2003', team: 'Milan', country: 'Italy'},
+    {season: '2003-2004', team: 'Porto', country: 'Portugal'},
+    {season: '2004-2005', team: 'Liverpool', country: 'England'},
+    {season: '2005-2006', team: 'Barcelona', country: 'Spain'},
+    {season: '2006-2007', team: 'Milan', country: 'Italy'},
+    {season: '2007-2008', team: 'Manchester United', country: 'England'},
+    {season: '2008-2009', team: 'Barcelona', country: 'Spain'},
+    {season: '2009-2010', team: 'Internazionale', country: 'Italy'},
+    {season: '2010-2011', team: 'Barcelona', country: 'Spain'},
+    {season: '2011-2012', team: 'Chelsea', country: 'England'},
+    {season: '2012-2013', team: 'Bayern Munich', country: 'Germany'},
+    {season: '2013-2014', team: 'Real Madrid', country: 'Spain'},
+    {season: '2014-2015', team: 'Barcelona', country: 'Spain'},
+    {season: '2015-2016', team: 'Real Madrid', country: 'Spain'},
+    {season: '2016-2017', team: 'Real Madrid', country: 'Spain'},
+  ];
+  var countryWin = 'Spain'  // should return 9
+  var country = 'Portugal'  // should return 1
+  var country = 'Russia'  // should return 0
+  
+  var challenge2 = document.getElementById('challenge2');
+
+  var winnerTotal = 0;
+ for (var j = 0; j < winnerList.length; j++){
+    if(winnerList[j].country === countryWin){
+        winnerTotal ++;
+    }
+  }
+console.log(winnerTotal);
+challenge2.innerHTML += "<h3>"+winnerTotal+"</h3>";
